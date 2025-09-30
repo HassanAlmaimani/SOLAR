e248f1228e1766940db10bd2fc50b5e0ef36396d
Hassane <halmaimani444@gmail.com>
2025-09-30 22:44:04 +0300

    neptune orbit added

---
+++ SOLAR/Assets/Scripts/UranusOrbit.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UranusOrbit : MonoBehaviour
+{
+    [Tooltip("Distance from the Sun")]
+    public float radius = 5f;
+
+    [Tooltip("Degrees per second")]
+    public float speed = 10f;
+
+    public Transform sun;          // drag the Sun here in the Inspector
+
+    private float angle;
+
+    void Start()
+    {
+        if (sun == null)           // safety: auto-find if you forgot to drag
+            sun = GameObject.FindWithTag("Sun").transform;
+    }
+
+    void Update()
+    {
+        angle += speed * Time.deltaTime;
+        angle = Mathf.Repeat(angle, 360f);
+
+        float rad = angle * Mathf.Deg2Rad;
+        Vector3 offset = new Vector3(
+            Mathf.Cos(rad) * radius,
+            0,
+            Mathf.Sin(rad) * radius);
+
+        transform.position = sun.position + offset;   // <-- relative orbit
+    }
+}
