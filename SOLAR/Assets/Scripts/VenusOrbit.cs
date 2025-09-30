dfab83951514408b436a7d4271d8d98192119542
Hassane <halmaimani444@gmail.com>
2025-09-30 22:22:09 +0300

    Earth orbit script added
    Earth now orbits relative to the Sun

--- SOLAR/Assets/Scripts/VenusOrbit.cs
+++ SOLAR/Assets/Scripts/VenusOrbit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MercuryOrbit : MonoBehaviour
+public class VenusOrbit : MonoBehaviour
 {
     [Tooltip("Distance from the Sun")]
     public float radius = 5f;
